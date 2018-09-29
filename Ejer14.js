2e4021f2883f90dd8f1ca65dbc44f7fa598d530c
Dalcoleas <00048015@uca.edu.sv>
2018-09-28 23:21:37 -0600

    Ejer14

---
+++ Ejer14.js
@@ -0,0 +1,79 @@
+var resultado;
+var convertor = {
+    convertir: function (medida, unidadDeMedida, unidadAconvertir, tipoDeMedida) {
+
+        if (tipoDeMedida == 'L') {
+
+            if (unidadDeMedida == 'm') {
+
+                if (unidadAconvertir == 'cm') {
+                    resultado = medida * 100;
+                    unidadAconvertir = "cm";
+                    return (resultado + unidadAconvertir);
+                }
+                else if (unidadAconvertir == 'km') {
+                    resultado = medida / 1000;
+                    unidadAconvertir = "km";
+                    return (resultado + unidadAconvertir);
+                }
+
+                else if (unidadAconvertir == 'ft') {
+                    resultado = medida * 3.28;
+                    unidadAconvertir = "ft";
+                    return (resultado + unidadAconvertir);
+                }
+            }
+
+            else if (unidadDeMedida == 'km') {
+
+                if (unidadAconvertir == 'm') {
+                    resultado = medida * 1000;
+                    unidadAconvertir = "m";
+                    return (resultado + unidadAconvertir);
+                }
+            }
+            else if (unidadDeMedida == 'ft')
+                if (unidadAconvertir == 'm') {
+                    resultado = medida * 0.3048;
+                    unidadAconvertir = "m";
+                    return (resultado + unidadAconvertir);
+                }
+        }
+
+        else if (tipoDeMedida == 'T') {
+            if (unidadDeMedida == 'c') {
+
+                if (unidadAconvertir == 'f') {
+                    resultado = (medida*(9/5)) + 32;
+                    unidadAconvertir = "f";
+                    return (resultado + unidadAconvertir);
+                }
+                else if (unidadAconvertir == 'k') {
+                    resultado = medida + 273.15;
+                    unidadAconvertir = "k";
+                    return (resultado + unidadAconvertir);
+                }
+            }
+
+            else if (unidadDeMedida == 'f') {
+
+                if (unidadAconvertir == 'c') {
+                    resultado = (medida-32)*(5/9);
+                    unidadAconvertir = "c";
+                    return (resultado + unidadAconvertir);
+                }
+            }
+            else if (unidadDeMedida == 'ft')
+                if (unidadAconvertir == 'm') {
+                    resultado = medida * 0.3048;
+                    unidadAconvertir = "m";
+                    return (resultado + unidadAconvertir);
+                }
+                else if (unidadAconvertir == 'k') {
+                    resultado = (medida + 459.67)*(5/9);
+                    unidadAconvertir = "k";
+                    return (resultado + unidadAconvertir);
+                }
+        }
+    }
+}
